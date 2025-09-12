d5ac56c84c38d2413784db7f1c2f599e4f773bb6
Rudraksh Mohanty <rudrakshmohanty05@gmail.com>
2025-09-12 08:35:49 +0530

    updates to the lab

---
+++ lab5/test-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function HelloWorld() {
+  return <h1 className="greeting">Hello, world!</h1>;
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>
+);
